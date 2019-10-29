617710961ee0b1082b303fb71eb532a528f0e40e
Dantir <45596475+Dantir@users.noreply.github.com>
2019-10-29 18:46:26 +0100

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Book
+    {
+        private int id;
+        private string title;
+        private string author;
+
+        public int Id { get => id; set => id = value; }
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+
+    }
+}
---
+++ Shelve.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Shelve
+    {
+        private Book[] Books;
+
+        public void setbooks()
+        {
+            Books = new Book[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Books[i] = new Book();
+                Books[i].Id = i;
+                Books[i].Title = "name " + i;
+                Books[i].Author = "auth " +i;
+            }
+        }
+        public void getbooks()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Books[i].Title);
+            }
+        }
+    }
+}
