271c328523955d5294956494d604817201bb36d3
Dantir <45596475+Dantir@users.noreply.github.com>
2019-10-29 17:54:05 +0100

    Add files via upload

---
+++ Dogs.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Dogs
+    {
+        private string name;
+        private string race;
+        private string color;
+        private double weight;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+        public string Race { get => race; set => race = value; }
+        public string Color { get => color; set => color = value; }
+        public double Weight { get => weight; /*set => weight = value;*/ }
+        public void setWeight(double newWeight)
+        {
+            if (newWeight < 0)
+                return;
+            weight = newWeight;
+        }
+        public int Age { get => age; /*set => age = value;*/ }
+        public void setAge(int newAge)
+        {
+            if (newAge < 0)
+                return;
+            age = newAge;
+        }
+
+        public string info()
+        {
+            return "\nName = " + Name + "\nShape = " + Race + "\nState = " + Color + "\nWeight = " + Weight + "\nAge = " + Age + "\nDog Age = " + Age*7 + "\n\n";
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dogs first = new Dogs();
+            first.Name = "Lucy";
+            first.Race = "Bulldog";
+            first.Color = "Brown";
+            first.setWeight(9.2);
+            first.setAge(5);
+            Console.Write("Dog 1" + first.info());
+            Dogs second = new Dogs();
+            second.Name = "Molly";
+            second.Race = "Labrador";
+            second.Color = "Light Brown";
+            second.setWeight(12.4);
+            second.setAge(3);
+            Console.Write("Dog 2" + second.info());
+            Console.ReadKey();
+        }
+    }
+}
