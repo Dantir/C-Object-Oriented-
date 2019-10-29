0fdc575a0f89a3169e8213ed278ed574c3e97609
Dantir <45596475+Dantir@users.noreply.github.com>
2019-10-29 17:51:57 +0100

    Add files via upload

---
+++ LightBulb.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LightBulb
+    {
+        private string color;
+        private string shape;
+        private string state;
+
+        public string Color { get => color; set => color = value; }
+        public string Shape { get => shape; set => shape = value; }
+        public string GetState()
+        {
+            return state;
+        }
+        public void SetState(string newState)
+        {
+            state = newState;
+            Console.WriteLine("\nNew state: " + state);
+        }
+        public string info()
+        {
+            return "\nColor = " + Color + "\nShape = " + Shape + "\nState = " + GetState() + "\n";
+        }
+    }
+}
