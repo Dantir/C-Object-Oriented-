a0614433e1ceea9fcbad10241ce0f9b47696a59c
Dantir <45596475+Dantir@users.noreply.github.com>
2019-11-07 18:25:21 +0100

    Add files via upload

---
+++ 07.11.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void CallComputerInfo(Computer comp)
+        {
+            comp.GetInfo();
+        }
+        static void Main(string[] args)
+        {
+            //Computer computer1 = new Computer(1920,1080);
+            //Computer computer2 = new Computer(Color.White, "Asus", 800, 400);
+            Computer computer1 = new Computer();
+            computer1.Colorname = Color.Black;
+            computer1.Brand = "Dell";
+            computer1.MonitorWidth = 1920;
+            computer1.MonitorHeight = 1080;
+            Computer Clone = computer1.Clone();
+            CallComputerInfo(computer1);
+            Console.WriteLine(computer1.Area());
+            CallComputerInfo(Clone);
+            //CallComputerInfo(computer2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Computer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum Color { Black = 1, Gray = 2, White = 3 }
+    public class Computer
+    {
+        /*public Computer(double MonitorWidth, double MonitorHeight)
+           {
+               Colorname = Color.Black;
+               Brand = "Undefined";
+               this.MonitorWidth = MonitorWidth;
+               this.MonitorHeight = MonitorHeight;
+           }*/
+        /*public Computer(Color Colorname, string Brand, double MonitorWidth, double MonitorHeight)
+        {
+            this.Colorname = Colorname;
+            this.Brand = Brand;
+            this.MonitorWidth = MonitorWidth;
+            this.MonitorHeight = MonitorHeight;
+        }*/
+        private Color colorname;
+        public Color Colorname
+        {
+            get { return colorname; }
+            set { colorname = value; }
+        }
+        private string brand;
+        public string Brand
+        {
+            get { return brand; }
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value))
+                { brand = value; }
+                else
+                { Console.WriteLine("Invalid data (String can't be empty!)"); }
+            }
+        }
+        private double monitorWidth;
+        public double MonitorWidth
+        {
+            get { return monitorWidth; }
+            set
+            {
+                if (value >= 0)
+                { monitorWidth = value; }
+                else
+                { Console.WriteLine("Invalid data (Number should be positive!)"); }
+            }
+        }
+        private double monitorHeight;
+        public double MonitorHeight
+        {
+            get { return monitorHeight; }
+            set
+            {
+                if (value >= 0)
+                { monitorHeight = value; }
+                else
+                { Console.WriteLine("Invalid data (Number should be positive!)"); }
+            }
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine("\nThe brand is " + Brand + "\nThe color is " + Colorname + "\nThe resolution is " + MonitorWidth + " x " + MonitorHeight);
+        }
+        public double Area()
+        {
+            return (MonitorWidth * MonitorHeight);
+        }
+        public Computer Clone()
+        {
+            Computer Clone = new Computer();
+            Clone.Colorname = Colorname;
+            Clone.Brand = Brand;
+            Clone.MonitorWidth = MonitorWidth;
+            Clone.MonitorHeight = MonitorHeight;
+            return Clone;
+        }
+    }
+}
