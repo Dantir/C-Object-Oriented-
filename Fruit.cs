298383324e4a49df7ee8678e2134455876a33bd4
Dantir <45596475+Dantir@users.noreply.github.com>
2019-11-23 23:06:24 +0100

    Add files via upload

---
+++ Fruit.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+    public abstract class Fruit
+    {
+        public string OriginCountry { get; protected set; }
+        public abstract bool ContainsJuice();
+        public double PricePerKg { get; protected set; }
+        public Fruit(double pricePerKg)
+        {
+            PricePerKg = pricePerKg;
+        }
+        public virtual double GetPricePerKg()
+        {
+            return PricePerKg;
+        }
+    }
+    public class Coconut : Fruit
+    {
+        public override bool ContainsJuice()
+        {
+            return true;
+        }
+        public Coconut(string originCountry, double pricePerKg) : base(pricePerKg)
+        {
+            OriginCountry = originCountry;
+        }
+    }
+    public class Banana : Fruit
+    {
+        public override bool ContainsJuice()
+        {
+            return false;
+        }
+        public override double GetPricePerKg()
+        {
+            return PricePerKg * 5;
+        }
+        public Banana(string originCountry, double pricePerKg) : base(pricePerKg)
+        {
+            OriginCountry = originCountry;
+        }
+    }
+    public class Orange : Fruit
+    {
+        public Season Season { get; private set; }
+        public override bool ContainsJuice()
+        {
+            return true;
+        }
+        public override double GetPricePerKg()
+        {
+            if (Season == Season.Autumn || Season == Season.Spring)
+            { return PricePerKg * 1.5; }
+            else if (Season == Season.Summer)
+            { return PricePerKg * 2; }
+            else
+                return PricePerKg;
+        }
+        public Orange(string originCountry, Season season, double pricePerKg) : base(pricePerKg)
+        {
+            OriginCountry = originCountry;
+            Season = season;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,14 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            LightBulb first = new LightBulb();
-            first.Color = "White";
-            first.Shape = "Candle";
-            first.SetState("Off");
-            Console.Write(first.info());
-            first.SetState("On");
-            Console.Write(first.info());
-            Console.ReadLine();
+            Fruit[] Array = new Fruit[5];
+            // Array[0] = new Orange(Season.Spring);
+            // Array[1] = new Banana("Canada");
+            // Array[2] = new Coconut("Kuala Lumpur");
+            Array[0] = new Banana("Poland", 8.2);
+            Array[1] = new Coconut("Poland", 19.9);
+            Array[2] = new Orange("Poland", Season.Spring, 10);
+            Array[3] = new Orange("Poland", Season.Summer, 10);
+            Array[4] = new Orange("Poland", Season.Winter, 10);
+
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine(Array[i].GetPricePerKg());
+            }
+            Console.ReadKey();
         }
     }
 }
