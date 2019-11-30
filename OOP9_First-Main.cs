0d5cd1f5b8e2d45327bd2380b7d22c676dd82120
Dantir <45596475+Dantir@users.noreply.github.com>
2019-11-30 20:51:42 +0100

    Add files via upload

---
+++ OOP9_First-Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OptionBase[] Array = new OptionBase[4];
+            Array[0] = new Monitor();
+            Array[1] = new File();
+            Array[2] = new SeparateFiles();
+            Array[3] = new UpperMonitor();
+            Console.WriteLine("Choose the option you want to use: (For your own safety don't write other numbers or especially letters)");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write(i + " - ");
+                Array[i].DisplayPurpose();
+            }
+            int index = Convert.ToInt32(Console.ReadLine());
+            string[] Strings = new string[5];
+            for (int i = 0; i < Strings.Length; i++)
+            {
+                Console.WriteLine("Please write the string number " + (i + 1));
+                Strings[i] = Console.ReadLine();
+            }
+            Array[index].WritingStrings(Strings);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP9_First-OptionBase.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP9
+{
+    public interface OptionBase
+    {
+        void DisplayPurpose();
+        void WritingStrings(string[] Strings);
+    }
+    public class Monitor : OptionBase
+    {
+        public void DisplayPurpose()
+        {
+            Console.WriteLine("Print lines to the screen");
+        }
+        public void WritingStrings(string[] Strings)
+        {
+            foreach (var element in Strings)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+    }
+    public class File : OptionBase
+    {
+        public void DisplayPurpose()
+        {
+            Console.WriteLine("Print lines to a file");
+        }
+        public void WritingStrings(string[] Strings)
+        {
+            System.IO.File.WriteAllLines(@"D:\Strings.txt", Strings);
+        }
+    }
+    public class SeparateFiles : OptionBase
+    {
+        public void DisplayPurpose()
+        {
+            Console.WriteLine("Print lines to seperate files");
+        }
+        public void WritingStrings(string[] Strings)
+        {
+                for (int i = 0; i < Strings.Length; i++)
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"D:\" + (i+1) + ".txt"))
+                file.WriteLine(Strings[i]);
+            }
+        }
+    }
+    public class UpperMonitor : OptionBase
+    {
+        public void  DisplayPurpose()
+        {
+            Console.WriteLine("Print lines to the screen in uppercase");
+        }
+        public void WritingStrings(string[] Strings)
+        {
+            foreach (var element in Strings)
+            {
+                Console.WriteLine(element.ToUpper());
+            }
+        }
+
+    }
+}
