177218d8e0c4e3d5f08d91c14adda2fa9ad20fa2
Dantir <45596475+Dantir@users.noreply.github.com>
2020-01-28 18:09:20 +0100

    Add files via upload

---
+++ OOP17_Integer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class UnsignedInt
+    {
+        int Integer;
+        public UnsignedInt(int Integer)
+        {
+            if (Integer < 0)
+                this.Integer = 0;
+            else
+                this.Integer = Integer;
+        }
+        public static UnsignedInt operator +(UnsignedInt a, UnsignedInt b)
+        {
+            return new UnsignedInt(a.Integer + b.Integer);
+        }
+        public static UnsignedInt operator +(UnsignedInt a, int b)
+        {
+            return new UnsignedInt(a.Integer + b);
+        }
+        public static UnsignedInt operator +(int b, UnsignedInt a)
+        {
+            return a + b;
+        }
+        public static UnsignedInt operator -(UnsignedInt a, UnsignedInt b)
+        {
+            return new UnsignedInt(a.Integer - b.Integer);
+        }
+        public static UnsignedInt operator -(UnsignedInt a, int b)
+        {
+            return new UnsignedInt(a.Integer - b);
+        }
+        public static UnsignedInt operator -(int b, UnsignedInt a)
+        {
+            return new UnsignedInt(b - a.Integer);
+        }
+        public static UnsignedInt operator *(UnsignedInt a, UnsignedInt b)
+        {
+            return new UnsignedInt(a.Integer * b.Integer);
+        }
+        public static UnsignedInt operator *(UnsignedInt a, int b)
+        {
+            return new UnsignedInt(a.Integer * b);
+        }
+        public static UnsignedInt operator *(int b, UnsignedInt a)
+        {
+            return a * b;
+        }
+        public static UnsignedInt operator /(UnsignedInt a, UnsignedInt b)
+        {
+            if (b.Integer == 0)
+            {
+                return new UnsignedInt(0);
+            }
+            else
+            return new UnsignedInt(a.Integer / b.Integer);
+        }
+        public static UnsignedInt operator /(UnsignedInt a, int b)
+        {
+            if (b == 0)
+            {
+                return new UnsignedInt(0);
+            }
+            return new UnsignedInt(a.Integer / b);
+        }
+        public static UnsignedInt operator /(int b, UnsignedInt a)
+        {
+            if (a.Integer == 0)
+            {
+                return new UnsignedInt(0);
+            }
+            return new UnsignedInt(b / a.Integer);
+        }
+        public override string ToString()
+        {
+            return Integer.ToString();
+        }
+    }
+}
---
+++ OOP17_Main.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnsignedInt int1 = new UnsignedInt(3) + new UnsignedInt(4);
+            Console.WriteLine(int1);
+            UnsignedInt int2 = new UnsignedInt(3) + new UnsignedInt(-2); //-2 can't be inside uint
+            Console.WriteLine(int2);
+            UnsignedInt int3 = new UnsignedInt(3) + 4;
+            Console.WriteLine(int3);
+            UnsignedInt int4 = 4 + new UnsignedInt(3);
+            Console.WriteLine(int4);
+            UnsignedInt int5 = new UnsignedInt(3) - new UnsignedInt(2);
+            Console.WriteLine(int5);
+            UnsignedInt int6 = new UnsignedInt(3) - new UnsignedInt(4);
+            Console.WriteLine(int6);
+            UnsignedInt int7 = new UnsignedInt(3) - 1;
+            Console.WriteLine(int7);
+            UnsignedInt int8 = 4 - new UnsignedInt(3);
+            Console.WriteLine(int8);
+            UnsignedInt int9 = new UnsignedInt(3) * 3;
+            Console.WriteLine(int9);
+            UnsignedInt int10 = -3 * new UnsignedInt(3);
+            Console.WriteLine(int10);
+            UnsignedInt int11 = new UnsignedInt(3) * new UnsignedInt(3);
+            Console.WriteLine(int11);
+            UnsignedInt int12 = new UnsignedInt(3) * new UnsignedInt(3);
+            Console.WriteLine(int12);
+            UnsignedInt int13 = new UnsignedInt(3) * new UnsignedInt(-3);
+            Console.WriteLine(int13);
+            UnsignedInt int14 = new UnsignedInt(3) / new UnsignedInt(-3);
+            Console.WriteLine(int14);
+            UnsignedInt int15 = new UnsignedInt(3) / 3;
+            Console.WriteLine(int15);
+            UnsignedInt int16 = 6 / new UnsignedInt(3);
+            Console.WriteLine(int16);
+            Console.ReadKey();
+        }
+    }
+}
